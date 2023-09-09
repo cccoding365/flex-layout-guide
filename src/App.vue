2976da0141ee84c8c202a293e0d37b2f8b6bd669
DoubledConG <992196871@qq.com>
2023-09-09 22:47:18 +0800

    新增flex-direction的四种情况

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
-    
+    <main>
+        <Demo1 />
+        <Demo2 />
+        <Demo3 />
+        <Demo4 />
+    </main>
 </template>
 
 <script setup>
+import Demo1 from './components/Demo1.vue';
+import Demo2 from './components/Demo2.vue';
+import Demo3 from './components/Demo3.vue';
+import Demo4 from './components/Demo4.vue';
 
 </script>
 
 <style lang="less" scoped>
-
+main {
+    max-width: 1024px;
+    width: 100%;
+    margin: auto;
+    padding: 16px;
+}
 </style>
