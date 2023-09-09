5906e1669d456d44c5a8cc9803f1aa53bd49ec3c
DoubledConG <992196871@qq.com>
2023-09-09 22:03:40 +0800

    清空布局

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import App from '@/App.vue'
+// 导入全局样式
 import '@/style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
