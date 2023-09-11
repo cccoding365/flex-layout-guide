ddcf76023957c8d74e80fe7dd43d710054032031
DoubledConG <992196871@qq.com>
2023-09-11 22:11:00 +0800

    新增align-content的六种情况

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
         <justifyContent />
         <alignItems />
         <flexWrap />
+        <alignContent />
     </main>
 </template>
 
@@ -11,7 +12,8 @@
 import flexDirection from '@/views/flex-direction/index.vue'
 import justifyContent from '@/views/justify-content/index.vue'
 import alignItems from '@/views/align-items/index.vue'
-import flexWrap from '@/views/flex-wrap/index.vue';
+import flexWrap from '@/views/flex-wrap/index.vue'
+import alignContent from '@/views/align-content/index.vue'
 
 </script>
 
---
+++ src/views/align-content/Demo2.vue
@@ -0,0 +1,14 @@
+<template>
+    <flexBox :num="27" />
+</template>
+
+<script setup>
+import flexBox from '@/components/flexBox.vue'
+</script>
+
+<style lang="less" scoped>
+.container {
+    flex-wrap: wrap;
+    align-content: flex-start;
+}
+</style>
